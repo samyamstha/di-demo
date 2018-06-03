57e3adf119a3f510ed48dc08499ba47028343dc0
samyamshrestha <samyamsth@gmail.com>
2018-06-03 14:56:21 -0400

    Added this as default profile as well.

--- src/main/java/com/samyam/didemo/services/PrimaryGreetingService.java
+++ src/main/java/com/samyam/didemo/services/PrimaryGreetingService.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-@Profile("en")
+@Profile({"en", "default"})
 @Primary
 public class PrimaryGreetingService implements GreetingService {
     @Override
