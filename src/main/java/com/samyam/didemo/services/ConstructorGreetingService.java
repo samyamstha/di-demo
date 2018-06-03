0aaa6a98d8d49132ef46336f1316c49f006be4cb
samyamshrestha <samyamsth@gmail.com>
2018-06-03 13:14:35 -0400

    Different implementations of the GreetingService Interface

---
+++ src/main/java/com/samyam/didemo/services/ConstructorGreetingService.java
@@ -0,0 +1,14 @@
+package com.samyam.didemo.services;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConstructorGreetingService implements GreetingService{
+
+
+    @Override
+    public String sayGreeting() {
+        return "Hello! I was injected by the constructor.";
+    }
+}
