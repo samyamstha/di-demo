683b824d464f63789017fc968431bcf04dc5dfad
samyamshrestha <samyamsth@gmail.com>
2018-06-03 14:36:28 -0400

    Added service implementation for spanish profile with @Profile("es")

---
+++ src/main/java/com/samyam/didemo/services/PrimarySpanishGreetingService.java
@@ -0,0 +1,16 @@
+package com.samyam.didemo.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile("es")
+@Primary
+public class PrimarySpanishGreetingService implements GreetingService{
+
+    @Override
+    public String sayGreeting() {
+        return "¡Hola! Esto es de la implementación del Servicio Principal de Saludo en Español. ";
+    }
+}
