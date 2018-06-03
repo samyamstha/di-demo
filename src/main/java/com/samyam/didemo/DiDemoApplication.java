730e00b4c9bdf5794dea91a40cb5f5be906aced6
samyamshrestha <samyamsth@gmail.com>
2018-06-03 12:44:08 -0400

    Wrote print statements to call the methods from the controllers implementing all three forms of dependency injection.

--- src/main/java/com/samyam/didemo/DiDemoApplication.java
+++ src/main/java/com/samyam/didemo/DiDemoApplication.java
@@ -1,6 +1,9 @@
 package com.samyam.didemo;
 
+import com.samyam.didemo.controllers.ConstructorInjectedController;
 import com.samyam.didemo.controllers.MyController;
+import com.samyam.didemo.controllers.PropertyInjectedController;
+import com.samyam.didemo.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -16,5 +19,10 @@ public class DiDemoApplication {
 
 		MyController controller = (MyController) ctx.getBean("myController"); //springboot defines the beans in camelcase.
 		System.out.println(controller.Hello());
+
+		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
+		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+		System.out.println(ctx.getBean(SetterInjectedController.class).sayHello());
+
 	}
 }
